0fa91b1d5f4c4fc83c2c07eb6efe110fae9ec141
ZhyvotovskyiSerhii <86567407+ZhyvotovskyiSerhii@users.noreply.github.com>
2022-01-20 17:04:40 +0200

    swiper-added

--- src/js/app.js
+++ src/js/app.js
@@ -2,26 +2,21 @@ import * as flsFunctions from "./modules/functions.js"
 
 flsFunctions.isWebp();
 
-import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper, { Navigation, Pagination } from "swiper";
 
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "vertical",
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
+// const swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
