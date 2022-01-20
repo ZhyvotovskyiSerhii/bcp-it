c55ba1219499dcb9a59b23c8a640cb9d4240f6f2
ZhyvotovskyiSerhii <86567407+ZhyvotovskyiSerhii@users.noreply.github.com>
2022-01-20 14:35:05 +0200

    need to fix

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,24 @@ flsFunctions.isWebp();
 
 import Swiper, { Navigation, Pagination } from "swiper";
 
-const swiper = new Swiper();
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
